e6f8ea41e69659466f3b67e7e6fa70a3f56392f8
Glenn Nelson <kitecamguy@gmail.com>
2017-08-13 15:23:00 -0700

    My Api and Config files

---
+++ Clock/ApiKeys-glenn.py
@@ -0,0 +1,5 @@
+# change this to your API keys
+# Weather Underground API key
+wuapi = '2b87fe7b21048478'
+# Google Maps API key
+googleapi = ''   # Empty string, optional -- with PiClock, you'll be ok
