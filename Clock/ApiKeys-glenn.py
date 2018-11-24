e91d67d49679292b758d20b605a6e183f648831d
Glenn Nelson <kitecamguy@gmail.com>
2018-11-23 17:44:42 -0800

    Update ApiKeys-glenn.py
    remove my mistake

--- Clock/ApiKeys-glenn.py
+++ Clock/ApiKeys-glenn.py
@@ -2,5 +2,4 @@
 # Weather Underground API key
 wuapi = '2b87fe7b21048478'
 # Google Maps API key
-#googleapi = 'AIzaSyBuvCn31hgFq1EcxDkWaDL4Vv9425lkriE'   # Empty string, optional -- with PiClock, you'll be ok
-googleapi = 'AIzaSyAqtBEQus1VdjgQ_DFgLRONRF8KmE3t2oo'
+googleapi = ''   # Empty string, optional -- with PiClock, you'll be ok
